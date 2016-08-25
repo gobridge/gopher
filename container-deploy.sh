cb1df9980ce8a59fb6b1aaf8fd38af6832a162fc
Florin Patan <florinpatan@gmail.com>
2016-08-25 13:57:17 +0100

    Fix deployment

--- container-deploy.sh
+++ container-deploy.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/account-auth.json
+
 sudo /opt/google-cloud-sdk/bin/gcloud docker push eu.gcr.io/${PROJECT_NAME}/bot
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl patch deployment gopher-slack-bot -p '{"spec":{"template":{"spec":{"containers":[{"name":"gopher-slack-bot","image":"eu.gcr.io/gopher-slack-bot/bot:'"$CIRCLE_SHA1"'"}]}}}}'
