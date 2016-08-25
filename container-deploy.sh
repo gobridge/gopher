15de82d9d0d56316354468d8ce490fa63aec281e
Florin Patan <florinpatan@gmail.com>
2016-08-26 00:51:14 +0100

    Proper rolling updates

--- container-deploy.sh
+++ container-deploy.sh
@@ -6,4 +6,4 @@ export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/account-auth.json
 
 sudo /opt/google-cloud-sdk/bin/gcloud docker push eu.gcr.io/${PROJECT_NAME}/bot
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl rolling-update --namespace=gopher-slack-bot --image=eu.gcr.io/gopher-slack-bot/bot:${CIRCLE_SHA1}
+kubectl rolling-update --namespace=gopher-slack-bot gopher-slack-bot --image=eu.gcr.io/gopher-slack-bot/bot:${CIRCLE_SHA1}
