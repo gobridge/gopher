3593dae0975814b5d12e59e13d017db60e647c9b
Florin Patan <florinpatan@gmail.com>
2016-08-27 03:11:53 +0100

    Ignore labels for now

--- container-deploy.sh
+++ container-deploy.sh
@@ -7,4 +7,4 @@ export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/account-auth.json
 sudo /opt/google-cloud-sdk/bin/gcloud docker push eu.gcr.io/${PROJECT_NAME}/bot
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl rolling-update --namespace=gopher-slack-bot gopher-slack-bot --image=eu.gcr.io/gopher-slack-bot/bot:${CIRCLE_SHA1}
-kubectl label --namespace=gopher-slack-bot pods --all --selector='app=gopher-slack-bot' version=${CIRCLE_SHA1} --overwrite=true
+#kubectl label --namespace=gopher-slack-bot pods --all --selector='app=gopher-slack-bot' version=${CIRCLE_SHA1} --overwrite=true
