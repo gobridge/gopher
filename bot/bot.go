d7baf31cf0dd7ae78fb86ff61283b0a397c716b0
Florin Patan <florinpatan@gmail.com>
2016-09-25 08:48:03 +0100

    More resources

--- bot/bot.go
+++ bot/bot.go
@@ -244,6 +244,11 @@ func (b *Bot) HandleMessage(event *slack.MessageEvent) {
 	}
 
 	if strings.Contains(eventText, strings.ToLower("@"+b.name)) || strings.Contains(eventText, strings.ToLower(b.id)) {
+		if strings.Contains(eventText, "package layout") {
+			b.PackageLayout(event)
+			return
+		}
+
 		if strings.Contains(eventText, "library for") ||
 			strings.Contains(eventText, "library in go for") ||
 			strings.Contains(eventText, "go library for") {
@@ -289,24 +294,32 @@ func (b *Bot) NewbieResources(event *slack.MessageEvent) {
 		Text: `First you should take the language tour: <http://tour.golang.org/>
 
 Then, you should visit:
- - <https://golang.org/doc/code.html> To learn how to organize your Go workspace
- - <https://golang.org/doc/effective_go.html> which would help you be more effective at writing Go
- - <https://golang.org/ref/spec> will help you learn more about the language itself
- - <https://golang.org/doc/#articles> For a lot more reading material
+ - <https://golang.org/doc/code.html> to learn how to organize your Go workspace
+ - <https://golang.org/doc/effective_go.html> be more effective at writing Go
+ - <https://golang.org/ref/spec> learn more about the language itself
+ - <https://golang.org/doc/#articles> a lot more reading material
 
 There are some awesome websites as well:
- - <https://blog.gopheracademy.com> Well great resources for Gophers in general
- - <http://gotime.fm> For a weekly podcast of Go awesomeness
- - <https://gobyexample.com> If you are looking for examples of how to do things in Go
- - <http://go-database-sql.org> If you are looking for how to use SQL databases in Go
- - <http://gophervids.appspot.com> For a list of Go related videos from various authors
+ - <https://blog.gopheracademy.com> great resources for Gophers in general
+ - <http://gotime.fm> awesome weekly podcast of Go awesomeness
+ - <https://gobyexample.com> examples of how to do things in Go
+ - <http://go-database-sql.org> how to use SQL databases in Go
+ - <http://gophervids.appspot.com> list of Go related videos from various authors
 
-Finally, <https://github.com/golang/go/wiki#learning-more-about-go> will give a list of more resources to learn Go`,
+If you prefer books, you can try these:
+ - <http://www.golangbootcamp.com/book>
+ - <http://gopl.io/>
+ - <https://www.manning.com/books/go-in-action> (if you e-mail @wkennedy at bill@ardanlabs.com you can get a free copy for being part of this Slack)
+
+If you want to learn how to organize your Go project, make sure to read: <https://medium.com/@benbjohnson/standard-package-layout-7cdbc8391fc1#.ds38va3pp>.
+Once you are acustomed to the language and syntax, you can read this series of articles for a walkthrough the various standard library packages: <https://medium.com/go-walkthrough>.
+
+Finally, <https://github.com/golang/go/wiki#learning-more-about-go> will give a list of even more resources to learn Go`,
 	}
 
 	params := slack.PostMessageParameters{AsUser: true}
 	params.Attachments = []slack.Attachment{newbieResources}
-	_, _, err := b.slackAPI.PostMessage(event.Channel, "Here are some resources you might want to check if you are new to Go:", params)
+	_, _, err := b.slackAPI.PostMessage(event.Channel, "Here are some resources you should check out if you are learning / new to Go:", params)
 	if err != nil {
 		b.log("%s\n", err)
 		return
@@ -545,6 +558,15 @@ func (b *Bot) ReplyFlipCoin(event *slack.MessageEvent) {
 	}
 	params := slack.PostMessageParameters{AsUser: true}
 	_, _, err = b.slackAPI.PostMessage(event.Channel, fmt.Sprintf("%s", result), params)
+	if err != nil {
+		b.log("%s\n", err)
+		return
+	}
+}
+
+func (b *Bot) PackageLayout(event *slack.MessageEvent) {
+	params := slack.PostMessageParameters{AsUser: true}
+	_, _, err := b.slackAPI.PostMessage(event.Channel, "This article will explain how to organize your Go packages <https://medium.com/@benbjohnson/standard-package-layout-7cdbc8391fc1#.ds38va3pp>", params)
 	if err != nil {
 		b.log("%s\n", err)
 		return
