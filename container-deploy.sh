4a4e2c069e1117968371d1fb215f23ef11ff0f8b
Florin Patan <florinpatan@gmail.com>
2016-10-11 22:47:36 +0100

    I should learn how to spell

--- container-deploy.sh
+++ container-deploy.sh
@@ -6,4 +6,4 @@ export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/account-auth.json
 
 sudo /opt/google-cloud-sdk/bin/gcloud docker push eu.gcr.io/${PROJECT_NAME}/bot
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl --namespace=gopher-slack-bot set image deployment/gopher-slack-bot gophe-slack-bot=eu.gcr.io/gopher-slack-bot/bot:${CIRCLE_SHA1}
+kubectl --namespace=gopher-slack-bot set image deployment/gopher-slack-bot gopher-slack-bot=eu.gcr.io/gopher-slack-bot/bot:${CIRCLE_SHA1}
