cfc56698aea2a0a42277d3fc88d16108fd8b6041
Florin Patan <florinpatan@gmail.com>
2016-11-12 12:18:04 +0000

    Simplify

--- gopher.go
+++ gopher.go
@@ -100,9 +100,7 @@ func main() {
 		},
 	}
 
-	if strings.HasPrefix(botName, "@") {
-		botName = botName[1:]
-	}
+	botName = strings.TrimPrefix(botName, "@")
 
 	anaconda.SetConsumerKey(twitterConsumerKey)
 	anaconda.SetConsumerSecret(twitterConsumerSecret)
@@ -145,18 +143,13 @@ func main() {
 	}()
 
 	go func() {
-		for {
-			select {
-			case msg := <-slackBotRTM.IncomingEvents:
-				switch message := msg.Data.(type) {
-				case *slack.MessageEvent:
-					go b.HandleMessage(message)
+		for msg := range slackBotRTM.IncomingEvents {
+			switch message := msg.Data.(type) {
+			case *slack.MessageEvent:
+				go b.HandleMessage(message)
 
-				case *slack.TeamJoinEvent:
-					go b.TeamJoined(message)
-				default:
-					_ = message
-				}
+			case *slack.TeamJoinEvent:
+				go b.TeamJoined(message)
 			}
 		}
 	}()
