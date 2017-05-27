b4e691d192da8a93401cf35f9f415955ab150aa3
Florin Patan <florinpatan@gmail.com>
2017-05-27 14:32:08 +0100

    Fix k8s

--- container-make.sh
+++ container-make.sh
@@ -5,15 +5,6 @@ sudo apt-get install python-openssl python3-openssl
 sudo /opt/google-cloud-sdk/bin/gcloud -q components update
 sudo /opt/google-cloud-sdk/bin/gcloud -q components update kubectl
 
-echo ${GCLOUD_SERVICE_KEY} | base64 --decode -i > ${HOME}/account-auth.json
-sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/account-auth.json
-
-sudo /opt/google-cloud-sdk/bin/gcloud -q config set project ${PROJECT_NAME}
-sudo /opt/google-cloud-sdk/bin/gcloud -q config set container/cluster ${CLUSTER_NAME}
-sudo /opt/google-cloud-sdk/bin/gcloud -q config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
-sudo /opt/google-cloud-sdk/bin/gcloud -q container clusters get-credentials $CLUSTER_NAME
-
-
 CONTAINER_NAME=eu.gcr.io/${PROJECT_NAME}/bot
 CONTAINER_TAG=${CIRCLE_SHA1}
 
