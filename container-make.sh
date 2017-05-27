4bc895058f1663654fffc6f9ac950675cd335d3f
Florin Patan <florinpatan@gmail.com>
2017-05-27 03:45:54 +0100

    Round Round

--- container-make.sh
+++ container-make.sh
@@ -4,8 +4,10 @@ sudo pip install pyopenssl
 sudo apt-get install python-openssl python3-openssl
 sudo /opt/google-cloud-sdk/bin/gcloud -q components update
 sudo /opt/google-cloud-sdk/bin/gcloud -q components update kubectl
-sudo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/account-auth.json
+
+echo ${GCLOUD_SERVICE_KEY} | base64 --decode -i > ${HOME}/account-auth.json
 sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/account-auth.json
+
 sudo /opt/google-cloud-sdk/bin/gcloud -q config set project ${PROJECT_NAME}
 sudo /opt/google-cloud-sdk/bin/gcloud -q config set container/cluster ${CLUSTER_NAME}
 sudo /opt/google-cloud-sdk/bin/gcloud -q config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
