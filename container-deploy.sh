dc954a92723feeeb49a822c30123b3de0ebf369f
Florin Patan <florinpatan@gmail.com>
2017-05-27 13:44:14 +0100

    please work

--- container-deploy.sh
+++ container-deploy.sh
@@ -2,5 +2,7 @@
 
 export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/account-auth.json
 
+echo ${GCLOUD_SERVICE_KEY} | base64 --decode -i > ${HOME}/account-auth.json
+sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/account-auth.json
 sudo /opt/google-cloud-sdk/bin/gcloud docker -- push eu.gcr.io/${PROJECT_NAME}/bot
 /opt/google-cloud-sdk/bin/kubectl --namespace=gopher-slack-bot set image deployment/gopher-slack-bot gopher-slack-bot=eu.gcr.io/gophers-slack-bot/bot:${CIRCLE_SHA1}
