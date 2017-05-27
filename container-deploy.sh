86aa1aeee9585cf4d4485875f06a492568ca31ed
Florin Patan <florinpatan@gmail.com>
2017-05-27 12:53:46 +0100

    Round, Round it goes

--- container-deploy.sh
+++ container-deploy.sh
@@ -3,5 +3,4 @@
 export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/account-auth.json
 
 sudo /opt/google-cloud-sdk/bin/gcloud docker -- push eu.gcr.io/${PROJECT_NAME}/bot
-sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl --namespace=gopher-slack-bot set image deployment/gopher-slack-bot gopher-slack-bot=eu.gcr.io/gophers-slack-bot/bot:${CIRCLE_SHA1}
+/opt/google-cloud-sdk/bin/kubectl --namespace=gopher-slack-bot set image deployment/gopher-slack-bot gopher-slack-bot=eu.gcr.io/gophers-slack-bot/bot:${CIRCLE_SHA1}
