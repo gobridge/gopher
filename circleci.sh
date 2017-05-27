867c807a7bef4366ef0d6e83c49e319b93e0f331
Florin Patan <florinpatan@gmail.com>
2017-05-27 03:05:30 +0100

    Even more CircleCI

--- circleci.sh
+++ circleci.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-pip install pyopenssl
-apt-get install python-openssl python3-openssl
-/opt/google-cloud-sdk/bin/gcloud -q components update
-/opt/google-cloud-sdk/bin/gcloud -q components update kubectl
-$GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/account-auth.json
-/opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/account-auth.json
-/opt/google-cloud-sdk/bin/gcloud -q config set project ${PROJECT_NAME}
-/opt/google-cloud-sdk/bin/gcloud -q config set container/cluster ${CLUSTER_NAME}
-/opt/google-cloud-sdk/bin/gcloud -q config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
-/opt/google-cloud-sdk/bin/gcloud -q container clusters get-credentials $CLUSTER_NAME
+sudo pip install pyopenssl
+sudo apt-get install python-openssl python3-openssl
+sudo /opt/google-cloud-sdk/bin/gcloud -q components update
+sudo /opt/google-cloud-sdk/bin/gcloud -q components update kubectl
+sudo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/account-auth.json
+sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/account-auth.json
+sudo /opt/google-cloud-sdk/bin/gcloud -q config set project ${PROJECT_NAME}
+sudo /opt/google-cloud-sdk/bin/gcloud -q config set container/cluster ${CLUSTER_NAME}
+sudo /opt/google-cloud-sdk/bin/gcloud -q config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
+sudo /opt/google-cloud-sdk/bin/gcloud -q container clusters get-credentials $CLUSTER_NAME
