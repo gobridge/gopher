753182d246ed4b9c6022288b569609ee3e69299a
Florin Patan <florinpatan@gmail.com>
2017-07-12 20:06:41 +0100

    Build Gopher with Go 1.9 Beta2

--- container-test.sh
+++ container-test.sh
@@ -13,7 +13,7 @@ docker run --rm \
     -e CI=true \
     -e GOPATH=${CONTAINER_PROJECT_GOPATH} \
     -w "${CONTAINER_PROJECT_DIR}" \
-    golang:1.8.3 \
+    golang:1.9beta2 \
     go test -v -race -tags netgo -installsuffix netgo ./... 2> output.log
 
 EXIT_CODE=$?
